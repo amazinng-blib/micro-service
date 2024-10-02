d31e53dc8b5895352f4658dbfacee77d4aa9dea6
Nwankwo Ernest Onyebuchi <nwankwoernest2020@gmail.com>
2024-10-02 13:39:26 +0100

    basic file and folder set-upo

--- user-service/index.js
+++ user-service/index.js
@@ -1,1 +1,12 @@
-ECHO is on.
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server listening on port : ${port}`));
